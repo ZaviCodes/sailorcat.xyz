c6ea115ab050ad539ded9ad86d5678dfa09a40bc
ZaviCodes <88913375+ZaviCodes@users.noreply.github.com>
2024-07-12 22:41:41 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Add any JavaScript effects here if needed
+});
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
